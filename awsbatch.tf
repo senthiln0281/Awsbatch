9e5b2db17a9561d97ff2d93a7175f1400370d129
senthiln0281 <senthiln0281@gmail.com>
2018-10-29 12:41:55 +0530

    cen

--- awsbatch.tf
+++ awsbatch.tf
@@ -92,6 +92,27 @@ resource "aws_batch_compute_environment" "asgen" {
   type = "MANAGED"
   depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
 }
+resource "aws_batch_compute_environment" "asgen1" {
+  compute_environment_name = "asgen1"
+  compute_resources {
+    instance_role = "${aws_iam_instance_profile.ecs_instance_role.arn}"
+    instance_type = [
+      "t2.micro",
+    ]
+    max_vcpus = 2
+    min_vcpus = 0
+    security_group_ids = [
+      "${aws_security_group.asgen.id}"
+    ]
+    subnets = [
+      "${aws_subnet.Pub_Sub.id}"
+    ]
+    type = "EC2"
+  }
+  service_role = "${aws_iam_role.aws_batch_service_role.arn}"
+  type = "MANAGED"
+  depends_on = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
+}
 resource "aws_batch_job_definition" "asgen" {
     name = "tf_test_batch_job_definition"
     type = "container"
