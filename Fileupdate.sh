00287c427158eb5ef0bdf9362936243e87dff7c9
senthiln0281 <senthiln0281@gmail.com>
2018-11-14 15:54:43 +0530

    update

--- Fileupdate.sh
+++ Fileupdate.sh
@@ -4,4 +4,5 @@ export PATH=/home/ec2-user/Awsbatch/.local/bin:$PATH
 #git fetch --all
 #git merge origin/Awsbatch
 #git pull --rebase https://github.com/senthiln0281/Awsbatch.git --verbose
-git pull https://github.com/senthiln0281/Awsbatch.git
+#git pull https://github.com/senthiln0281/Awsbatch.git
+git pull --rebase origin
