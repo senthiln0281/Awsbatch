88b1d7462b062c1e275ef5dd1a4eecd323fffdd2
senthiln0281 <senthiln0281@gmail.com>
2018-11-15 20:01:22 +0530

    update

--- Fileupdatetest.sh
+++ Fileupdatetest.sh
@@ -6,11 +6,10 @@ git clone https://github.com/senthiln0281/Awsbatch.git
 #Pull the repo to get latest updates
 git pull --rebase origin 
 
-#List the files
-
-ls -l
-
 #Copy the files from jenkins server to remote server
 
-scp test2.txt ec2-user@10.0.19.82:/home/ec2-user.test
-
+scp test2.txt ec2-user@ 10.0.19.82:/home/ec2-user
+#ssh 
+#wget https://raw.githubusercontent.com/senthiln0281/Awsbatch/master/awsbatch.tf
+pwd
+ls -l
