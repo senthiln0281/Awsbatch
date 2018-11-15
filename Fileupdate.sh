f9de5d8290a6d3b32f04cd35808b5852ac5a3b39
senthiln0281 <senthiln0281@gmail.com>
2018-11-15 16:21:15 +0530

    update

--- Fileupdate.sh
+++ Fileupdate.sh
@@ -5,9 +5,10 @@ export PATH=/home/ec2-user/Awsbatch/.local/bin:$PATH
 #git merge origin/Awsbatch
 #git pull --rebase https://github.com/senthiln0281/Awsbatch.git --verbose
 #git pull https://github.com/senthiln0281/Awsbatch.git
-#git pull --rebase origin 
+git pull --rebase origin 
 #ssh senthil@172.31.28.122 pqsswd Spam99$$
-#scp test2.txt senthil@172.31.28.122:/home/senthil
-wget https://raw.githubusercontent.com/senthiln0281/Awsbatch/master/awsbatch.tf
+scp test2.txt ec2-user@ 10.0.19.82:/home/ec2-user
+#ssh 
+#wget https://raw.githubusercontent.com/senthiln0281/Awsbatch/master/awsbatch.tf
 pwd
 ls -l
