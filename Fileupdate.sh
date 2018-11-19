e6803d5a93885332c0db8df7d71d3458b3e20395
senthiln0281 <senthiln0281@gmail.com>
2018-11-19 14:51:04 +0530

    multifile

--- Fileupdate.sh
+++ Fileupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Clone the repo
-#git clone https://github.com/senthiln0281/Awsbatch.git
+git clone https://github.com/senthiln0281/Awsbatch.git
 
 #git clone -branch Fileupdate https://github.com/senthiln0281/Awsbatch.git
 
@@ -25,17 +25,20 @@ ls -l
 
 scp Fileupdatetest.sh ec2-user@10.0.19.82:/home/ec2-user/test
 
+
+scp -r ec2-user@10.0.19.82:/home/ec2-user/test
+
 #cd /var/lib/jenkins/workspace/FileUpdate
 
 #rm -f *
 
-git clone -branch test https://github.com/senthiln0281/Awsbatch.git
+#git clone -branch test https://github.com/senthiln0281/Awsbatch.git
 
 #git pull --rebase origin
 
-ls -l
+#ls -l
 
 
-scp as.txt ec2-user@10.0.19.82:/home/ec2-user/test1
+#scp as.txt ec2-user@10.0.19.82:/home/ec2-user/test1
 
 
