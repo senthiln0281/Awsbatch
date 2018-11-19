d80e0a0c4c02999e0aa8ed8dfc42eea6a73b8427
senthiln0281 <senthiln0281@gmail.com>
2018-11-19 17:18:57 +0530

    multifile

--- Fileupdate.sh
+++ Fileupdate.sh
@@ -36,7 +36,8 @@ pwd
 
 scp fileupdate.zip ec2-user@10.0.19.82:/home/ec2-user/test
 
-ssh ec2-user@10.0.19.82 && cd /home/ec2-user/test | yes | sudo unzip fileupdate.zip
+ssh ec2-user@10.0.19.82 
+cd /home/ec2-user/test | yes | sudo unzip fileupdate.zip
 
 #yes | unzip fileupdate.zip
 
