fab4724ce68d3f96a14ba1f9c6f6341323a269bc
senthiln0281 <senthiln0281@gmail.com>
2018-11-19 18:55:38 +0530

    multifile

--- filemove.sh
+++ filemove.sh
@@ -10,6 +10,9 @@ git pull --rebase origin
 
 ls -l
 
+cd test/
+
+
 zip test.zip *.*
 
 scp test.zip ec2-user@10.0.19.82:/home/ec2-user/test
@@ -18,5 +21,19 @@ ssh -t -t ec2-user@10.0.19.82 << EOF
 echo $HOSTNAME
 cd /home/ec2-user/test
 yes | sudo unzip test.zip
+exit
+EOF
+
+cd update/
+
+
+zip update.zip *.*
+
+scp update.zip ec2-user@10.0.19.82:/home/ec2-user/update
+
+ssh -t -t ec2-user@10.0.19.82 << EOF
+echo $HOSTNAME
+cd /home/ec2-user/update
+yes | sudo unzip update.zip
 exit
 EOF
