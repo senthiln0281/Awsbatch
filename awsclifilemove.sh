baea8e1ce00134a94d500e7a41f211e0d93650e7
senthiln0281 <senthiln0281@gmail.com>
2018-11-27 12:10:01 +0530

    ssh

--- awsclifilemove.sh
+++ awsclifilemove.sh
@@ -15,15 +15,15 @@ cd Awsbatch/
 
 #Zip the file from above directory
 
-zip Awsbatch.zip *.*
+#zip Awsbatch.zip *.*
 
 #Create temp S3 bucket
 
-aws s3 create-bucket --bucket 4977041 --region us-west-2
+#aws s3 create-bucket --bucket 4977041 --region us-west-2
 
 #Copy the files to S3
 
-aws s3 cp Awsbatch.zip s3://4977041/Awsbatch.zip
+#aws s3 cp Awsbatch.zip s3://4977041/Awsbatch.zip
 
 
 
