1fdbb14f9a6d85382c8b58077643816c6bc947e3
senthiln0281 <senthiln0281@gmail.com>
2018-11-29 15:02:15 +0530

    rssh

--- awsclifilemove.sh
+++ awsclifilemove.sh
@@ -9,26 +9,7 @@ git pull --rebase origin
 
 ls -l
 
-#Move into dags directory to zip files
 
-cd update/
-
-ls -l
-
-#Zip the file from above directory
-
-zip update.zip *.*
-
-
-~/.local/bin/aws --version
-
-#Create temp S3 bucket
-
-aws s3 mb s3://4977041 --region us-west-2
-
-#Copy the files to S3
-
-aws s3 cp Awsbatch.zip s3://4977041/update.zip
 
 
 
