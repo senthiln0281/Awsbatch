b5857f51c2d6aa7230e806e471611f6077dc7f4b
senthiln0281 <senthiln0281@gmail.com>
2018-11-29 15:00:30 +0530

    rssh

--- awsclifilemove.sh
+++ awsclifilemove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Clone the repo
-git clone https://gitlab-aid.devops.amgen.com/infosearch/veeva-orchestration.git
+git clone https://github.com/senthiln0281/Awsbatch.git
 
 #Pull the repo to get latest updates
 git pull --rebase origin 
@@ -24,11 +24,11 @@ zip update.zip *.*
 
 #Create temp S3 bucket
 
-#aws s3 create-bucket --bucket 4977041 --region us-west-2
+aws s3 mb s3://4977041 --region us-west-2
 
 #Copy the files to S3
 
-#aws s3 cp Awsbatch.zip s3://497704/update.zip
+aws s3 cp Awsbatch.zip s3://4977041/update.zip
 
 
 
